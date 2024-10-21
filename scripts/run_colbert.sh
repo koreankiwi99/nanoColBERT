b02d3bee64817a599161ffc131b940bb9d34c946
koreankiwi99 <salgu@snu.ac.kr>
2024-10-21 21:10:48 +0200

    Add files via upload

--- scripts/run_colbert.sh
+++ scripts/run_colbert.sh
@@ -1,22 +1,28 @@
 ## make large tsv file to memory-mapping file for efficient pytorch indexing
-python tsv2mmap.py
-
+python tsv2mmap.py \
+    --triple_path /content/drive/MyDrive/DIS/train_triple.tsv\ #TODO
+    #--doc_max_len 512
+    #--query_max_len 200
+  
 ## train
 python train_colbert.py
 
 ## using trained model to produce document embedding
-accelerate launch doc2embedding.py --pretrained_model_path wandb/latest-run/files/step-400000 --output_dir embedding/colbert
+accelerate launch doc2embedding.py \
+    --pretrained_model_path wandb/latest-run/files/step-4000 \ #TODO
+    --output_dir embedding/colbert
+    --collection_path corpus.json #TODO
 
 ## build faiss index for efficient retrieval
 python build_index.py --embedding_dir embedding/colbert --output_path embedding/colbert/ivfpq.faiss.index
 
 ## conduct retrieval
-python retrieve.py \
-    --embedding_dir embedding/colbert \
-    --faiss_index_path embedding/colbert/ivfpq.faiss.index \
-    --pretrained_model_path wandb/latest-run/files/step-400000 \
-    --query_path data/queries.dev.small.tsv \
-    --output_path ranking.tsv
+#python retrieve.py \
+#    --embedding_dir embedding/colbert \
+#    --faiss_index_path embedding/colbert/ivfpq.faiss.index \
+#    --pretrained_model_path wandb/latest-run/files/step-400000 \
+#    --query_path data/queries.dev.small.tsv \
+#    --output_path ranking.tsv
 
 ## calculate score
-python score.py --qrel_path data/qrels.dev.small.tsv --ranking_path ranking.tsv
+#python score.py --qrel_path data/qrels.dev.small.tsv --ranking_path ranking.tsv
